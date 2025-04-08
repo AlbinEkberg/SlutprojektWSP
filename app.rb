ca244c28083690c444f6d5107b5ff1d30b3fef2d
AlbinEkberg <albin.ekberg@elev.ga.ntig.se>
2025-04-08 19:49:51 +0200

    1

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'slim'
 require 'sqlite3'
@@ -8,231 +10,228 @@ require_relative './model/model.rb'
 
 enable :sessions
 
-def generate_chest() # väljer en kista baserat på sällsynthet högre sällsynthet = bättre kista
-    db = connect_to_db()
-    chests = db.execute("SELECT * FROM chests")
-    total_rarity = chests.sum { |chest| chest["rarity"] }
-    random_value = rand(total_rarity)
-
-    cumulative_rarity = 0
-    selected_chest = chests.find do |chest|
-        cumulative_rarity += chest["rarity"]
-        random_value < cumulative_rarity
-    end
-
-    session[:current_chest] = selected_chest
-end
-
-def generate_card() # väljer ett kort baserat på sällsynthet högre sällsynthet = bättre kort
-    db = connect_to_db()
-    reverse_chest_rarity = 1 + (50 / session[:current_chest]["rarity"])
-
-    p reverse_chest_rarity
-
-    selection = rand(1..(100 - reverse_chest_rarity)) # håll på ett tag med det här, inte en bra lösning men fungerar
-
-    case selection
-    when 1
-        rarity = "legendary"    # 1% baschans
-        amount = 1
-    when 2..12
-        rarity = "epic"         # 10% baschans
-        amount = rand(1..5) + (reverse_chest_rarity / 2).to_i
-    when 13..33
-        rarity = "rare"         # 20% baschans
-        amount = rand(5..20) + reverse_chest_rarity
-    when 34..70
-        rarity = "common"       # 35% baschans
-        amount = rand(20..100) + (reverse_chest_rarity * 2)
-    else
-        rarity = "gold"
-        amount = rand(100..1000) * reverse_chest_rarity
-    end
-    cards_of_rarity = db.execute("SELECT * FROM cards WHERE rarity = ?",rarity)
-    selected_card = cards_of_rarity.sample
-
-    return { card: selected_card, amount: amount }
-end
+include Model
 
 before do
-    protected_routes = ["/collection/card", "/collection/chest", "/market/index"] # routes som kräver inloggning
+  protected_routes = ["/card", "/market"]
 
-    if protected_routes.include?(request.path_info) && session[:id] == nil
-        flash[:need_login] = "You need to log in to see this"
-        redirect('/showlogin')
-    end
+  if protected_routes.include?(request.path_info) && session[:id].nil?
+    flash[:need_login] = "You need to log in to see this"
+    redirect('/showlogin')
+  end
 end
 
+##
+# GET /
+#
+# Displays the homepage. If no chest exists in the session, one is generated.
+#
+# @return [Slim::Template] Renders the index view.
 get('/') do
-    if session[:current_chest] == nil
-        generate_chest()
-    end
-    slim(:index)
+  generate_chest() if session[:current_chest].nil?
+  slim(:index)
 end
 
+##
+# GET /showlogin
+#
+# Displays the login page.
+#
+# @return [Slim::Template] Renders the login form.
 get('/showlogin') do
-    slim(:'users/login')
+  slim(:'users/login')
 end
 
+##
+# GET /users/register
+#
+# Displays the user registration form.
+#
+# @return [Slim::Template] Renders the user registration form.
 get('/users/register') do
-    slim(:'users/register')
+  slim(:'users/new')
 end
 
-get('/collection/card') do
-    db = connect_to_db()
-    cards = db.execute("SELECT * FROM cards WHERE id != ?",44)
-    cards_rel = db.execute("SELECT * FROM users_cards_rel WHERE user_id = ? AND card_id != ?",[session[:id],44])
-    slim(:'collection/card',locals:{cards:cards,cards_rel:cards_rel})
+##
+# GET /card
+#
+# Displays all cards and the current user's card collection.
+#
+# @return [Slim::Template] Renders the cards overview.
+get('/card') do
+  cards = select_all_cards()
+  cards_rel = select_all_users_cards(session[:id])
+  slim(:'card/index', locals: { cards: cards, cards_rel: cards_rel })
 end
 
+##
+# GET /users/logout
+#
+# Logs the user out, destroys the session, and redirects to the homepage.
+#
+# @return [Redirect] Redirects to homepage.
 get('/users/logout') do
-    session.destroy
-    flash[:logout] = "You have been logged out"
-    redirect('/')
+  session.destroy
+  flash[:logout] = "You have been logged out"
+  redirect('/')
 end
 
-get('/collection/chest') do
-    slim(:'collection/chest')
+##
+# GET /market
+#
+# Displays the market page.
+#
+# @return [Slim::Template] Renders the market view.
+get('/market') do
+  slim(:'market/index')
 end
 
-get('/market/index') do
-    slim(:'market/index')
+##
+# GET /card/show
+#
+# Displays a single card. Used when opening a chest.
+#
+# @return [Slim::Template] Renders card show view without layout.
+get('/card/show') do
+  slim(:'card/show', layout: false)
 end
 
-get('/showchest') do
-    slim(:'users/chest_unlock', layout: false)
-end
-
+##
+# GET /admin
+#
+# Displays the admin panel if the user is an admin.
+#
+# @return [Slim::Template] Renders the admin dashboard.
 get('/admin') do
-    check_admin()
-
-    users = select_all("users")
-
-    slim(:'admin/index', layout: false, locals: { users: users })
+  check_admin()
+  users = select_all_users()
+  slim(:'admin/index', layout: false, locals: { users: users })
 end
 
+##
+# POST /admin/:id/delete
+#
+# Deletes a user with the given ID. Only accessible by admin.
+#
+# @param [Integer] id The ID of the user to delete.
+# @return [Redirect] Redirects back to the admin panel.
 post('/admin/:id/delete') do
-    check_admin()
-
-    delete("users", params[:id].to_i)
-
-    redirect('/admin')
+  check_admin()
+  delete_user(params[:id].to_i)
+  redirect('/admin')
 end
 
+##
+# POST /chest/open
+#
+# Opens a chest, generates cards, and stores them in the session.
+# Redirects to card reveal or homepage.
+#
+# @return [Redirect] Redirects to card reveal or homepage depending on chest status.
 post('/chest/open') do
-    if session[:current_chest] != nil # körs första gången
-        case session[:current_chest]["rarity"] # olika mängd kort beroende på sällsynthet
-        when 1..9
-            card_amount = 4
-        when 10..99
-            card_amount = 3
-        else
-            card_amount = 2
-        end
-
-        gotten_cards = []
-        db = connect_to_db()
-        i = card_amount
-        while i > 0
-            gotten_cards << generate_card()
-            if session[:id] != nil
-                if gotten_cards[-1][:card]["id"] != 44 # om det är en guldklimp så ska den inte läggas till i databasen
-                    if db.execute("SELECT card_id FROM users_cards_rel WHERE user_id = ?",session[:id]).include?({ "card_id" => gotten_cards[-1][:card]["id"] })
-                        db.execute("UPDATE users_cards_rel SET amount = amount + ? WHERE user_id = ? AND card_id = ?",[gotten_cards[-1][:amount],session[:id],gotten_cards[-1][:card]["id"]])
-
-                    else
-                        db.execute("INSERT INTO users_cards_rel (user_id, card_id, amount, level, for_sale) VALUES (?,?,?,?,?)",[session[:id],gotten_cards[-1][:card]["id"],gotten_cards[-1][:amount],gotten_cards[-1][:card]["base_lvl"],0])
-                    end
-                else
-                    db.execute("UPDATE users SET balance = balance + ? WHERE id = ?",[gotten_cards[-1][:amount], session[:id]])
-                end
-            end
-            i -= 1
-        end
-        
-
-        session[:current_chest] = nil
-        session[:cards_left] = card_amount
-        session[:gotten_cards] = gotten_cards
-        session[:balance] = db.execute("SELECT balance FROM users WHERE id = ?",session[:id]).first["balance"] if session[:id] != nil
-    end
-
-    if session[:cards_left] > 0 # körs varje gång du klickar för att vissa nästa kort
-        session[:current_card] = session[:gotten_cards][session[:cards_left] - 1]
-        session[:cards_left] -= 1
-        redirect('/showchest')
+  if session[:current_chest]
+    case session[:current_chest]["rarity"]
+    when 1..9
+      card_amount = 4
+    when 10..99
+      card_amount = 3
     else
-        redirect('/')
+      card_amount = 2
     end
+
+    gotten_cards = []
+    db = connect_to_db()
+    card_amount.times do
+      card = generate_card()
+      gotten_cards << card
+      add_card_to_user([card], session[:id]) if session[:id]
+    end
+
+    session[:current_chest] = nil
+    session[:cards_left] = gotten_cards.length
+    session[:gotten_cards] = gotten_cards
+    session[:balance] = reload_balance(session[:id])
+  end
+
+  if session[:cards_left].to_i > 0
+    session[:current_card] = session[:gotten_cards][session[:cards_left] - 1]
+    session[:cards_left] -= 1
+    redirect('/card/show')
+  else
+    redirect('/')
+  end
 end
 
+##
+# POST /users/login
+#
+# Handles user login.
+#
+# @param [String] username
+# @param [String] password
+# @return [Redirect] Redirects to home or admin depending on user type, or back to login on failure.
 post('/users/login') do
-    username = params[:username]
-    password = params[:password]
-    if username == "" || password == ""
-        session[:login_error_message] = "You need to fill in all fields"
-        redirect('/showlogin')
-    end
-    db = connect_to_db()
-    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+  username = params[:username]
+  password = params[:password]
 
-    if result == nil
-        session[:login_error_message] = "No such user"
-        redirect('/showlogin')
-    end
+  if username.empty? || password.empty?
+    session[:login_error_message] = "You need to fill in all fields"
+    redirect('/showlogin')
+  end
 
-    pwdigest = result["pwdigest"]
-    id = result["id"]
+  result = select_user(username).first
 
-    if BCrypt::Password.new(pwdigest) == password
-        session[:id] = id
-        session[:username] = username
-        session[:balance] = result["balance"]
-        flash[:login] = "Successfully logged in as #{username}"
-        if result["admin"] != nil
-            redirect('/admin')
-        else
-            redirect('/')
-        end
-    else
-        session[:login_error_message] = "Wrong password"
-        redirect('/showlogin')
-    end
+  if result.nil?
+    session[:login_error_message] = "No such user"
+    redirect('/showlogin')
+  end
+
+  pwdigest = result["pwdigest"]
+  id = result["id"]
+
+  if BCrypt::Password.new(pwdigest) == password
+    session[:id] = id
+    session[:username] = username
+    session[:balance] = result["balance"]
+    flash[:login] = "Successfully logged in as #{username}"
+    redirect(result["admin"] ? '/admin' : '/')
+  else
+    session[:login_error_message] = "Wrong password"
+    redirect('/showlogin')
+  end
 end
 
+##
+# POST /users/new
+#
+# Handles new user registration.
+#
+# @param [String] username
+# @param [String] password
+# @param [String] password_confirm
+# @return [Redirect] Redirects to home or back to registration with error.
 post('/users/new') do
-    username = params[:username]
-    password = params[:password]
-    password_confirm = params[:password_confirm]
-    db = connect_to_db()
+  username = params[:username]
+  password = params[:password]
+  password_confirm = params[:password_confirm]
 
-    if username == "" || password == "" || password_confirm == ""
-        session[:register_error_message] = "You need to fill in all fields"
-        redirect('/users/register')
-    end
+  if username.empty? || password.empty? || password_confirm.empty?
+    session[:register_error_message] = "You need to fill in all fields"
+    redirect('/users/register')
+  end
 
-    if db.execute("SELECT * FROM users WHERE username = ?", username).any?
-        session[:register_error_message] = "Username already taken"
-        redirect('/users/register')
-    end
+  if select_user(username).any?
+    session[:register_error_message] = "Username already taken"
+    redirect('/users/register')
+  end
 
-    if (password == password_confirm)
-        password_digest = BCrypt::Password.create(password)
-        db = SQLite3::Database.new('db/clash.db')
-        db.execute("INSERT INTO users (username, pwdigest, balance) VALUES (?,?,?)",[username,password_digest,1000])
-        
-        db = connect_to_db() # connect again to get the new user
+  result = password_digest(username, password, password_confirm)
 
-        result = db.execute("SELECT * FROM users WHERE username = ?", username).first
-
-        session[:id] = result["id"]
-        session[:balance] = result["balance"]
-        session[:username] = username
-        flash[:login] = "Successfully logged in as #{username}"
-        redirect('/')
-    else
-        session[:register_error_message] = "Passwords do not match"
-        redirect('/users/register')
-    end
+  if result
+    session[:id] = result["id"]
+    session[:balance] = result["balance"]
+    session[:username] = username
+    flash[:login] = "Successfully logged in as #{username}"
+    redirect('/')
+  end
 end
-
--- model/model.rb
+++ model/model.rb
@@ -1,28 +1,183 @@
-def connect_to_db()
-    db = SQLite3::Database.new('db/clash.db')
-    db.results_as_hash = true
-    return db
-end
+module Model
 
-def check_correct_user()
-
-end
-
-def select_all(where)
-    db = connect_to_db()
-    result = db.execute("SELECT * FROM #{where}")
-    return result
-end
-
-def delete(where, id)
-    db = connect_to_db()
-    db.execute("DELETE FROM #{where} WHERE id = ?", id)
-end
-
-def check_admin()
-    db = connect_to_db()
-    admins = db.execute("SELECT id FROM users WHERE admin IS NOT NULL")
-    if admins.any? { |admin| admin["id"] != session[:id] }
-        redirect('/showlogin')
+    ##
+    # Connects to the SQLite database.
+    #
+    # @return [SQLite3::Database] the database connection
+    def connect_to_db
+      db = SQLite3::Database.new('db/clash.db')
+      db.results_as_hash = true
+      return db
     end
-end
+  
+    ##
+    # Selects a user by username.
+    #
+    # @param [String] username the username to look up
+    # @return [Array<Hash>] array of user records matching the username
+    def select_user(username)
+      db = connect_to_db
+      db.execute("SELECT * FROM users WHERE username = ?", username)
+    end
+  
+    ##
+    # Adds a card to a user or adds to balance if the card is gold (ID 44).
+    #
+    # @param [Array<Hash>] gotten_cards the array of gotten cards, last element is used
+    # @param [Integer] id the user's ID
+    # @return [void]
+    def add_card_to_user(gotten_cards, id)
+      db = connect_to_db
+      card = gotten_cards[-1][:card]
+      amount = gotten_cards[-1][:amount]
+  
+      if card["id"] != 44
+        existing_cards = db.execute("SELECT card_id FROM users_cards_rel WHERE user_id = ?", id)
+        if existing_cards.include?({ "card_id" => card["id"] })
+          db.execute("UPDATE users_cards_rel SET amount = amount + ? WHERE user_id = ? AND card_id = ?",
+                     [amount, id, card["id"]])
+        else
+          db.execute("INSERT INTO users_cards_rel (user_id, card_id, amount, level, for_sale) VALUES (?,?,?,?,?)",
+                     [id, card["id"], amount, card["base_lvl"], 0])
+        end
+      else
+        db.execute("UPDATE users SET balance = balance + ? WHERE id = ?", [amount, id])
+      end
+    end
+  
+    ##
+    # Reloads and returns the current balance for a user.
+    #
+    # @param [Integer] id the user's ID
+    # @return [Integer, nil] the balance or nil if user not found
+    def reload_balance(id)
+      db = connect_to_db
+      db.execute("SELECT balance FROM users WHERE id = ?", id).first["balance"] if id
+    end
+  
+    ##
+    # Selects all users from the database.
+    #
+    # @return [Array<Hash>] all user records
+    def select_all_users
+      db = connect_to_db
+      db.execute("SELECT * FROM users")
+    end
+  
+    ##
+    # Selects all cards excluding the gold card (ID 44).
+    #
+    # @return [Array<Hash>] card records
+    def select_all_cards
+      db = connect_to_db
+      db.execute("SELECT * FROM cards WHERE id != ?", 44)
+    end
+  
+    ##
+    # Selects all user-card relationships for a user, excluding gold.
+    #
+    # @param [Integer] id the user's ID
+    # @return [Array<Hash>] cards belonging to the user
+    def select_all_users_cards(id)
+      db = connect_to_db
+      db.execute("SELECT * FROM users_cards_rel WHERE user_id = ? AND card_id != ?", [id, 44])
+    end
+  
+    ##
+    # Creates a new user if passwords match and returns the new user.
+    #
+    # @param [String] username the desired username
+    # @param [String] password the password
+    # @param [String] password_confirm the confirmation password
+    # @return [Hash, nil] the new user record or nil on mismatch
+    def password_digest(username, password, password_confirm)
+      db = connect_to_db
+      if password == password_confirm
+        digest = BCrypt::Password.create(password)
+        db.execute("INSERT INTO users (username, pwdigest, balance) VALUES (?,?,?)", [username, digest, 1000])
+  
+        db = connect_to_db # reconnect to fetch the new user
+        db.execute("SELECT * FROM users WHERE username = ?", username).first
+      else
+        session[:register_error_message] = "Passwords do not match"
+        redirect('/users/register')
+      end
+    end
+  
+    ##
+    # Deletes a user by ID.
+    #
+    # @param [Integer] id the user's ID
+    # @return [void]
+    def delete_user(id)
+      db = connect_to_db
+      db.execute("DELETE FROM users WHERE id = ?", id)
+    end
+  
+    ##
+    # Checks if the current session user is an admin.
+    # Redirects to login if not an admin.
+    #
+    # @return [void]
+    def check_admin
+      db = connect_to_db
+      admins = db.execute("SELECT id FROM users WHERE admin IS NOT NULL")
+      unless admins.any? { |admin| admin["id"] == session[:id] }
+        redirect('/showlogin')
+      end
+    end
+  
+    ##
+    # Generates a chest for the session based on rarity weights.
+    #
+    # @return [void]
+    def generate_chest
+      db = connect_to_db
+      chests = db.execute("SELECT * FROM chests")
+      total_rarity = chests.sum { |chest| chest["rarity"] }
+      random_value = rand(total_rarity)
+  
+      cumulative_rarity = 0
+      selected_chest = chests.find do |chest|
+        cumulative_rarity += chest["rarity"]
+        random_value < cumulative_rarity
+      end
+  
+      session[:current_chest] = selected_chest
+    end
+  
+    ##
+    # Generates a card based on the current chest rarity and weighted randomness.
+    #
+    # @return [Hash] card data and amount => { card: Hash, amount: Integer }
+    def generate_card
+      db = connect_to_db
+      reverse_chest_rarity = 1 + (50 / session[:current_chest]["rarity"])
+  
+      selection = rand(1..(100 - reverse_chest_rarity))
+  
+      case selection
+      when 1
+        rarity = "legendary"
+        amount = 1
+      when 2..12
+        rarity = "epic"
+        amount = rand(1..5) + (reverse_chest_rarity / 2).to_i
+      when 13..33
+        rarity = "rare"
+        amount = rand(5..20) + reverse_chest_rarity
+      when 34..70
+        rarity = "common"
+        amount = rand(20..100) + (reverse_chest_rarity * 2)
+      else
+        rarity = "gold"
+        amount = rand(100..1000) * reverse_chest_rarity
+      end
+  
+      cards_of_rarity = db.execute("SELECT * FROM cards WHERE rarity = ?", rarity)
+      selected_card = cards_of_rarity.sample
+  
+      { card: selected_card, amount: amount }
+    end
+  end
+  
